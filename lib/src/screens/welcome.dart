8a7cb20982b66a830b246aa80d10990b10ccd883
Igor Ming de Mesquita <igormmesquita@ymail.com>
2019-07-09 19:56:45 -0300

    implement location ask into the app

--- lib/src/screens/welcome.dart
+++ lib/src/screens/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather/src/screens/weather.dart';
 import 'package:weather/src/widgets/input_text.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Welcome extends StatelessWidget {
   static const String routeName = '/welcome';
@@ -9,6 +10,47 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future<Position> _askLocationPermission() async {
+      try {
+        Position position = await Geolocator()
+            .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        print(position);
+        return position;
+      } catch (e) {
+        return null;
+      }
+    }
+
+    void _navigate() {
+      Navigator.of(context).pushNamed(
+        Weather.routeName,
+        arguments: Weather(
+          userName: this._controller.text,
+        ),
+      );
+    }
+
+    Future<void> _showModal() {
+      return showDialog<void>(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext buildContext) {
+            return AlertDialog(
+              title: Text("Permission not granted"),
+              content: SingleChildScrollView(
+                child: Text(
+                    "Make sure you have your GPS turned on, and allow the app location permission."),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("OK"),
+                  onPressed: Navigator.of(context).pop,
+                )
+              ],
+            );
+          });
+    }
+
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
@@ -16,13 +58,26 @@ class Welcome extends StatelessWidget {
         child: InputText(
           'Type your name',
           controller: this._controller,
-          onConfirm: () {
-            Navigator.of(context).pushNamed(
-              Weather.routeName,
-              arguments: Weather(
-                userName: this._controller.text,
-              ),
-            );
+          onConfirm: () async {
+            GeolocationStatus geolocationStatus =
+                await Geolocator().checkGeolocationPermissionStatus();
+
+            print(geolocationStatus);
+
+            switch (geolocationStatus) {
+              case GeolocationStatus.denied:
+              case GeolocationStatus.disabled:
+                var location = await _askLocationPermission();
+                if (location == null) {
+                  _showModal();
+                } else {
+                  _navigate();
+                }
+                break;
+              case GeolocationStatus.granted:
+              default:
+                _navigate();
+            }
           },
         ),
       ),
